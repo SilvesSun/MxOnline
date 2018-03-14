93a21915006406bf3af3cd41f17107dceb194ba7
sksun <sk.sun@r-pac.com.cn>
2018-03-14 11:33:44 +0800

    base model

---
+++ apps/courses/adminx.py
@@ -0,0 +1,33 @@
+# coding:utf-8
+__date__ = '2018/3/13 10:12'
+
+from .models import *
+import xadmin
+
+
+class CourseAdmin(object):
+    list_display = ['name', 'desc', 'detail', 'degree']
+    search_fields = ['name', 'desc', 'detail', 'degree']
+    list_filter = ['name', 'desc', 'detail', 'degree']
+
+
+class LessonAdmin(object):
+    list_display = ['course', 'name', 'add_time', ]
+    search_fields = ['course', 'name', 'add_time', ]
+    list_filter = ['course__name', 'name', 'add_time', ]
+
+
+class VideoAdmin(object):
+    list_display = ['lesson', 'name', 'add_time']
+    search_fields = ['lesson', 'name']
+    list_filter = ['lesson', 'name', 'add_time']
+
+
+class CourseResourceAdmin(object):
+    pass
+
+
+xadmin.site.register(Course, CourseAdmin)
+xadmin.site.register(Lesson, LessonAdmin)
+xadmin.site.register(Video, VideoAdmin)
+xadmin.site.register(CourseResource, CourseResourceAdmin)
